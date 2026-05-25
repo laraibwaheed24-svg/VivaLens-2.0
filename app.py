06d9a5016f82cede0eec1e4548669157d787c761
laraibwaheed24-svg <laraibwaheed24@gmail.com>
2026-05-25 21:41:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -79,15 +79,6 @@ h1, h2, h3 {
 """, unsafe_allow_html=True)
 
 
-h1, h2, h3 {
-    color: #f9fafb;
-}
-
-.metric-card:hover {
-    transform: translateY(-3px);
-    transition: 0.3s;
-    box-shadow: 0 10px 20px rgba(0,0,0,0.3);
-}
 
 
 # =====================================================
