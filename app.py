7195c3c37d7c51cd11d9b077dbc872a4cc057cde
laraibwaheed24-svg <laraibwaheed24@gmail.com>
2026-05-25 21:36:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,18 @@ st.set_page_config(
 
 st.markdown("""
 <style>
+
+h1, h2, h3 {
+    color: #f9fafb;
+}
+
+.metric-card:hover {
+    transform: translateY(-3px);
+    transition: 0.3s;
+    box-shadow: 0 10px 20px rgba(0,0,0,0.3);
+}
+
+
 .block-container {
     padding-top: 2rem;
     padding-bottom: 2rem;
