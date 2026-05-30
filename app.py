4918696a17ada0ad91931b86f65bb9facd3d24c1
laraibwaheed24-svg <laraibwaheed24@gmail.com>
2026-05-30 16:37:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -820,32 +820,7 @@ if st.session_state.mode == "University Final Exam":
 
     st.markdown("---")
 
-    # =========================
-    # ADMIN DASHBOARD (ONLY HERE)
-    # =========================
-    st.subheader("📊 Examiner Dashboard")
 
-    if os.path.exists("student_results.xlsx"):
-
-        df = pd.read_excel("student_results.xlsx")
-
-        col1, col2, col3 = st.columns(3)
-
-        with col1:
-            st.metric("Total Students", len(df))
-
-        with col2:
-            st.metric("Passed", len(df[df["Status"] == "PASS"]))
-
-        with col3:
-            st.metric("Failed", len(df[df["Status"] == "FAIL"]))
-
-        st.markdown("---")
-
-        st.dataframe(df, use_container_width=True)
-
-    else:
-        st.info("No records yet")
     st.sidebar.error(f"⚠️ Warnings: {st.session_state.warnings}/3")
     
 
